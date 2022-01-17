007a0de78dd7f7e37ac3819717a1b9e313f9c338
Fern Alves <fernandapontual5@gmail.com>
2022-01-17 17:47:02 +0100

    client and server side connected

--- quote-test/client/src/components/Loading/Home.js
+++ quote-test/client/src/components/Loading/Home.js
@@ -1,50 +1,56 @@
-import './App.css';
+
 import react, {
-    useEffect,
-    useState,
-    createRef
-} from 'react';
-import axios from "axios"
-
-
-function App() {
-  const [quote, setQuote] = useState(null)
-  const [name, setName] = useState({name: null, show: false})
+     useEffect,
+     useState,
+     createRef
+ } from 'react';
+ import axios from "axios"
  
-// Finishing up client side, then get my API from my server.
-    const getQuote = async () => {
-        let response = await axios.get("https://goquotes-api.herokuapp.com/api/v1/random?count=1")
-        setQuote(response.data)
-    }
-
-    useEffect(() => {
-      getQuote()
-    }, [])
-
-    if (!quote) {
-        return ( 
-            <h1> loading... </h1>
-        )
-    }
-
-    console.log(name)
-
-    return (
-        <div>
-            <h1> Quote generator</h1>
-            <input type="text" onChange={(e) => setName({value: e.target.value, show: false})}></input>
-            <button onClick={()=> {
-                getQuote()
-                setName({value: name.value, show: true})
-            }}>Click Me!</button>
-            <div>
-            <h1> Dear,  {(name.show ? name.value : '')} </h1>
-            </div>
-            <div>
-                <h1> {quote.quotes[0].text} </h1>
-            </div>
-        </div> 
-    );
-}
-
-export default App;
+ 
+ 
+ function App(){
+   const [quote, setQuote] = useState(null)
+   const [name, setName] = useState({name: null, show: false})
+   
+     const getQuote = async () => {
+         let response = await axios.get("http://localhost:5005/api/name")
+         setQuote(response.data)
+     }
+ 
+ 
+ const myForm = () => {
+     return ( 
+         <div>
+         <h1> Quote generator</h1>
+         <input type="text" onChange={(e) => setName({value: e.target.value, show: false})}></input>
+         <button onClick={async ()=> {
+             // getQuote()
+             setName({value: name.value, show: true})
+             let response = await axios.post("http://localhost:5005/api/name", {name: name.value})
+             setQuote(response.data)
+         }}>Click Me!</button>
+         </div>
+     )
+ }
+ 
+     useEffect(() => {
+     //   getQuote()
+     }, [])
+ 
+     if(!quote) {
+         return myForm()
+     }
+ 
+     return (
+         <div>
+         {myForm()}
+             <h1> Dear,   {quote.quotes[0].author} </h1>
+             
+             <div>
+                 {quote.quotes[0].text}
+             </div>
+         </div> 
+     );
+ }
+ 
+ export default App;
