ca9bfe1887b16679de4fe0e494566c8359b07b08
Fern Alves <fernandapontual5@gmail.com>
2022-01-17 17:43:59 +0100

    axios dependency and post API

--- quote-test/server/routes/index.js
+++ quote-test/server/routes/index.js
@@ -1,10 +1,26 @@
 const router = require("express").Router();
+const axios = require("axios") 
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-// You put the next routes here 👇
-// example: router.use("/auth", authRoutes)
+
+
+router.post("/name", async (req, res, next) => {
+ let {name} = req.body
+  let response = await axios.get("https://goquotes-api.herokuapp.com/api/v1/random?count=1")
+ response.data.quotes[0].author = name
+  res.json(response.data)
+});
+
+
+
+
+
+
+//{quote.quotes[0].text}
+
 
 module.exports = router;
+
