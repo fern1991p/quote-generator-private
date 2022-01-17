8a44d1a9b10aa13ed5dc95a994fb1cec83c941d8
Fern Alves <fernandapontual5@gmail.com>
2022-01-17 17:33:23 +0100

    client side working concept

--- quote-test/client/src/components/Loading/Home.js
+++ quote-test/client/src/components/Loading/Home.js
@@ -1,51 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-import react, { useEffect, useState } from 'react';
+import react, {
+    useEffect,
+    useState,
+    createRef
+} from 'react';
 import axios from "axios"
 
+
 function App() {
-
-//using state to update my userimput. Print will be deleted soon. 
   const [quote, setQuote] = useState(null)
-  const [data,setData] = useState(null)
-  const [print, setPrint] = useState(false)
+  const [name, setName] = useState({name: null, show: false})
+ 
+// Finishing up client side, then get my API from my server.
+    const getQuote = async () => {
+        let response = await axios.get("https://goquotes-api.herokuapp.com/api/v1/random?count=1")
+        setQuote(response.data)
+    }
 
-//checking if my API is giving my quote. Will change to my localhost, once I start the server side. 
-useEffect(()=>{
-const getData = async () => {
-     let response = await axios.get("https://goquotes-api.herokuapp.com/api/v1/random?count=1")
-setQuote(response.data)
+    useEffect(() => {
+      getQuote()
+    }, [])
+
+    if (!quote) {
+        return ( 
+            <h1> loading... </h1>
+        )
+    }
+
+    console.log(name)
+
+    return (
+        <div>
+            <h1> Quote generator</h1>
+            <input type="text" onChange={(e) => setName({value: e.target.value, show: false})}></input>
+            <button onClick={()=> {
+                getQuote()
+                setName({value: name.value, show: true})
+            }}>Click Me!</button>
+            <div>
+            <h1> Dear,  {(name.show ? name.value : '')} </h1>
+            </div>
+            <div>
+                <h1> {quote.quotes[0].text} </h1>
+            </div>
+        </div> 
+    );
 }
-getData()
-}, [])
 
-
-if (!quote){
-return(
-<h1> loading.. </h1>
-)}
-
-
-function getData(value){
-setData(value.target.value)
-setPrint(false)
-}
-
-//My quote generator. To be moved into a function. 
-return (
-<div>
-<h1> Quote generator</h1>
-<input type="text" onChange={getData}></input>
-<button  onClick={()=>setPrint(true)}>Click Me!</button>
-<div>
-  {print?
-    <h1> Dear, {data}  </h1>
-    :null}
-</div>
-<div>
-{quote.quotes[0].text}
-</div>
-</div>
-  );
-}
 export default App;
